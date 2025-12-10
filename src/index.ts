9992a1fa954f36173e0a15810aa6ea1c8b178883
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2025-12-10 11:24:23 +0100

    chore: use version from package.json in cli

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 #!/usr/bin/env bun
 import { Command } from "commander";
+import { version } from "../package.json";
 import { configCommand } from "./commands/config.js";
 import { writeCommand } from "./commands/write.js";
 
 const program = new Command();
 
-program.name("jot").description("AI Research Assistant CLI").version("0.0.2");
+program.name("jot").description("AI Research Assistant CLI").version(version);
 
 program.addCommand(configCommand);
 program.addCommand(writeCommand);
