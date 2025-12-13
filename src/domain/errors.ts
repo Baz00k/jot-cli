da660d67b4b34a97c1a59ddbeaae163870854ac0
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2025-12-13 19:56:30 +0100

    refactor: move errors to folder

---
+++ src/domain/errors.ts
@@ -0,0 +1,15 @@
+import { Data } from "effect";
+
+export class ConfigReadError extends Data.TaggedError("ConfigReadError")<{
+    readonly cause: unknown;
+}> {}
+
+export class ConfigWriteError extends Data.TaggedError("ConfigWriteError")<{
+    readonly cause: unknown;
+}> {}
+
+export class ConfigDirError extends Data.TaggedError("ConfigDirError")<{
+    readonly cause?: unknown;
+}> {}
+
+export class UserCancel extends Data.TaggedError("UserCancel") {}
